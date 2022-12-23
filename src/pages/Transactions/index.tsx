9ee81db3d065a5e0a404779ceec737118de6b08c
Igor Trentini <igortrentini.2004@gmail.com>
2022-12-23 15:40:16 -0300

    feat: Adicionado tabela de transacaoes

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Header } from '../../components/layout/Header';
 import { Summary } from '../../components/layout/Summary';
+import { PriceHighlight, TransactionsContainer, Transactionstable } from './style';
 
 export const Transactions: FC = () => {
     return (
@@ -8,6 +9,30 @@ export const Transactions: FC = () => {
             <Header />
 
             <Summary />
+
+            <TransactionsContainer>
+                <Transactionstable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">-R$ 12.000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </Transactionstable>
+            </TransactionsContainer>
         </div>
     );
 };
